d3149f2dbc00cb531e5fcdea9583a849443196fe
jedidah <nzilajedidah@gmail.com>
2025-01-08 22:51:23 +0300

    combined all components in one file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Project from './pages/Project';
-import Contact from './pages/Contact';
+import MainComponent from "./components"; // Import the component from the components folder
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <div className="bg-gray-100 dark:bg-gray-900 min-h-screen">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Project" element={<Project />} />
-          <Route path="/Contact" element={<Contact />} />
-
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <MainComponent />
+    </div>
   );
-};
+}
 
 export default App;
